bb3bd101155a542b83dd7ec00e2f7ff86c6c17de
Joost <jstrmk@gmail.com>
2025-12-14 21:06:02 +0100

    vite: reset to default ignore list

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
-import typo3, { getDefaultIgnoreList } from 'vite-plugin-typo3';
+import typo3 from 'vite-plugin-typo3';
 
 export default defineConfig({
 	plugins: [typo3(), tailwindcss()],
-	server: {
-		watch: {
-			ignored: [...getDefaultIgnoreList(), '**/*.{html,php,typoscript,yaml,tsconfig,ts,js}'],
-		},
-	},
+	// server: {
+	// 	watch: {
+	// 		ignored: [...getDefaultIgnoreList(), '**/*.{html,php,typoscript,yaml,tsconfig,ts,js}'],
+	// 	},
+	// },
 });
