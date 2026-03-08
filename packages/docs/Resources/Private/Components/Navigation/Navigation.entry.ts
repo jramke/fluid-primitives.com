bf40381827b46a0391b480bdf1ee605cf7988ed9
jramke <jstrmk@gmail.com>
2026-03-08 23:11:44 +0100

    feat: make scrollbar state persistend across page visits note that we cant just set the stored values from the session storage on the elements directly and instead need to use these classes with important, because otherwise zag would override these fixed values again with its initial render where the values still arent computed correctly. thats why we need these classes and just simply remove them when we can garantuee that zag calculates all values by itself

--- packages/docs/Resources/Private/Components/Navigation/Navigation.entry.ts
+++ packages/docs/Resources/Private/Components/Navigation/Navigation.entry.ts
@@ -1,14 +1,51 @@
 import { getHydrationData, mount } from 'fluid-primitives';
 import { Dialog } from 'fluid-primitives/dialog';
+import { ScrollArea } from 'fluid-primitives/scroll-area';
 
-(() => {
-	mount('navigation', ({ props }) => {
-		const dialogProps = getHydrationData('dialog', 'nav-drawer-' + props.id)!.props;
-		const dialog = new Dialog(dialogProps);
-		dialog.init();
+mount('navigation', ({ props }) => {
+	const dialogProps = getHydrationData('dialog', 'nav-drawer-' + props.id)!.props;
+	const dialog = new Dialog(dialogProps);
+	dialog.init();
 
-		window.addEventListener('resize', () => {
-			dialog.api.setOpen(false);
+	const sidebarScrollAreaProps = getHydrationData('scroll-area', 'sidebar-' + props.id)!.props;
+	const scrollArea = new ScrollArea(sidebarScrollAreaProps);
+	scrollArea.init();
+
+	// Cleanup the fixed values when the machine computed all values needed for layout styles
+	setTimeout(() => {
+		scrollArea.getElements('thumb').forEach(thumbEl => {
+			thumbEl.classList.remove('!transform-[var(--sidebar-stored-transform)]');
+			thumbEl.classList.remove('!h-[var(--sidebar-stored-thumb-height)]');
 		});
+		document.documentElement.style.removeProperty('--sidebar-stored-transform');
+		document.documentElement.style.removeProperty('--sidebar-stored-thumb-height');
+	}, 100);
+
+	window.addEventListener('beforeunload', () => {
+		sessionStorage.setItem(
+			'sidebar-scroll-top',
+			scrollArea.getElement('viewport')?.scrollTop.toString() ?? '0'
+		);
+
+		const hovered = scrollArea.api.getScrollbarState({ orientation: 'vertical' }).hovering;
+		if (hovered) {
+			sessionStorage.setItem('sidebar-hovered', '1');
+			sessionStorage.setItem(
+				'sidebar-thumb-height',
+				scrollArea.getElement('root')?.style.getPropertyValue('--thumb-height') ?? ''
+			);
+			sessionStorage.setItem(
+				'sidebar-thumb-transform',
+				scrollArea.getElements('thumb')[0]?.style.getPropertyValue('transform') ?? ''
+			);
+		} else {
+			sessionStorage.removeItem('sidebar-hovered');
+			sessionStorage.removeItem('sidebar-thumb-height');
+			sessionStorage.removeItem('sidebar-thumb-transform');
+		}
 	});
-})();
+
+	window.addEventListener('resize', () => {
+		dialog.api.setOpen(false);
+	});
+});
