284c0f394f5752564a246153250fe34bb8c29d00
jramke <jstrmk@gmail.com>
2026-09-16 11:36:41 +0200

    Update examples

--- packages/docs/Resources/Private/Components/TransformExample/TransformExample.entry.ts
+++ packages/docs/Resources/Private/Components/TransformExample/TransformExample.entry.ts
@@ -1,15 +1,13 @@
-import { getHydrationData, mountAll } from 'fluid-primitives';
+import { mount, mountAll } from 'fluid-primitives';
 import { Input } from 'fluid-primitives/input';
 
 mountAll('transform-example', () => {
-    const data = getHydrationData('input', 'transform-example');
-    if (!data) return;
-
-    const input = new Input({
-        ...data.props,
-        transform: value => value.toUpperCase(),
+    mount('input', 'transform-example-input', ({ props }) => {
+        const input = new Input({
+            ...props,
+            transform: value => value.toUpperCase(),
+        });
+        input.init();
+        return input;
     });
-
-    input.init();
-    return input;
 });
